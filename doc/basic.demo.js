664336fe7273112b0bbdd09c6b6ea1d86a693641
Nimo <nimo.jser@gmail.com>
2018-03-20 11:54:22 +0800

    compile

---
+++ doc/basic.demo.js
@@ -0,0 +1,2 @@
+var jsonModif = require('json-modif')
+jsonModif.create('class.user.title', 'nimo')
---
+++ test/basic.test.js
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import jsonModif from "../lib/index"
+import extend from "extend"
+describe('create', () => {
+    it('object', () => {
+        expect(
+            jsonModif.create('class.user.title', 'nimo')
+        ).to.eql(
+            { class: { user: { title: 'nimo' } } }
+        )
+    })
+
+    it('object.array.object', () => {
+        expect(
+            JSON.stringify(jsonModif.create('class.user[0].title', 'nimo'))
+        ).to.eql(
+            JSON.stringify(
+                {"class":{"user":[{"title":"nimo"}]}}
+            )
+        )
+        expect(
+            JSON.stringify(jsonModif.create('class.user[1].title', 'nimo'))
+        ).to.eql(
+            JSON.stringify(
+                {"class":{"user":[undefined, {"title":"nimo"}]}}
+            )
+        )
+        expect(
+            JSON.stringify(jsonModif.create('class.user[1][4].title', 'nimo'))
+        ).to.eql(
+            JSON.stringify(
+                {"class":{"user":[undefined,[undefined,undefined,undefined,undefined,{"title":"nimo"}]]}}
+            )
+        )
+    })
+
+})

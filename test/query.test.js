522119ed1b81e7915136f80e97416c6ffcb8c706
Nimo <nimo.jser@gmail.com>
2018-03-27 14:34:31 +0800

    queryAll (backups)

--- test/query.test.js
+++ test/query.test.js
@@ -83,82 +83,115 @@ describe('query', () => {
             undefined
         )
     })
-    it('object.array.object', () => {
-        expect(
-            jsonModif.query('class.queue[1].title', data)
-        ).to.eql(
-            'nico'
-        )
-        expect(
-            jsonModif.query('class.queue[2][0].title', data)
-        ).to.eql(
-            'sam'
-        )
-    })
-    it('object.array[query].object', () => {
-        expect(
-            jsonModif.query('class.list[{id:"bh4ebrbvdsdqw"}].name', data)
-        ).to.eql(
-            'nimo'
-        )
-    })
-    it('object.array[query].object.array[query].object', () => {
-        expect(
-            jsonModif.query('class.list[{id:"egu8y23hgf3wef"}].data.array[{id:"ashfuewgfwef"}].name', data)
-        ).to.eql(
-            'Jack'
-        )
-        expect(
-            jsonModif.query('class.list[{name:"nimo", age: 24}].id', data)
-        ).to.eql(
-            'bh4ebrbvdsdqw'
-        )
-    })
-    it('location object.array[query].object 1', () => {
-        expect(
-            JSON.stringify(
-                jsonModif.query('class.list[{id:"egu8y23hgf3wef"}].data.array[{id:"ashfuewgfwef"}].name', data, {
-                    complete: true
-                })
-            )
-        ).to.eql(
-             '{"location":["class","list[0]","data","array[0]","name"],"value":"Jack"}'
-        )
-    })
-    it('location object.array[query].object 2', () => {
-        expect(
-            JSON.stringify(
-                jsonModif.query('class.list[{id:"bh4ebrbvdsdqw"}].data.array[{id:"gw34gobgjieuwcv"}].name', data, {
-                    complete: true
-                })
-            )
-        ).to.eql(
-            '{"location":["class","list[1]","data","array[2]","name"],"value":"Jack"}'
-        )
-    })
-    it('query all', () => {
-        expect(
-            JSON.stringify(
-                jsonModif.query('class.list[{name:"nimo"}]', data, {
-                    all: true
-                })
-            )
-        ).to.eql(
-            JSON.stringify(
-                [{"id":"bh4ebrbvdsdqw","name":"nimo","age":24,"data":{"array":[{"id":"egvwegewfwd","name":"Jack"},{"id":"safwegweg","name":"Jack"},{"id":"gw34gobgjieuwcv","name":"Jack"}]}},{"id":"b4wg4w3g3g434g","name":"nimo"}]
-            )
-        )
-    })
-    it('query all complete', () => {
-        expect(
-            JSON.stringify(
-                jsonModif.query('class.list[{name:"nimo"}]', data, {
-                    all: true,
-                    complete: true
-                })
-            )
-        ).to.eql(
-            '{"location":[["class","list[1]"],["class","list[2]"]],"value":[{"id":"bh4ebrbvdsdqw","name":"nimo","age":24,"data":{"array":[{"id":"egvwegewfwd","name":"Jack"},{"id":"safwegweg","name":"Jack"},{"id":"gw34gobgjieuwcv","name":"Jack"}]}},{"id":"b4wg4w3g3g434g","name":"nimo"}]}'
-         )
-    })
+    // it('object.array.object', () => {
+    //     expect(
+    //         jsonModif.query('class.queue[1].title', data)
+    //     ).to.eql(
+    //         'nico'
+    //     )
+    //     expect(
+    //         jsonModif.query('class.queue[2][0].title', data)
+    //     ).to.eql(
+    //         'sam'
+    //     )
+    // })
+    // it('object.array[query].object', () => {
+    //     expect(
+    //         jsonModif.query('class.list[{id:"bh4ebrbvdsdqw"}].name', data)
+    //     ).to.eql(
+    //         'nimo'
+    //     )
+    // })
+    // it('object.array[query].object.array[query].object', () => {
+    //     expect(
+    //         jsonModif.query('class.list[{id:"egu8y23hgf3wef"}].data.array[{id:"ashfuewgfwef"}].name', data)
+    //     ).to.eql(
+    //         'Jack'
+    //     )
+    //     expect(
+    //         jsonModif.query('class.list[{name:"nimo", age: 24}].id', data)
+    //     ).to.eql(
+    //         'bh4ebrbvdsdqw'
+    //     )
+    // })
+    // it('location object.array[query].object 1', () => {
+    //     expect(
+    //         JSON.stringify(
+    //             jsonModif.query('class.list[{id:"egu8y23hgf3wef"}].data.array[{id:"ashfuewgfwef"}].name', data, {
+    //                 complete: true
+    //             })
+    //         )
+    //     ).to.eql(
+    //          '{"location":["class","list[0]","data","array[0]","name"],"value":"Jack"}'
+    //     )
+    // })
+    // it('location object.array[query].object 2', () => {
+    //     expect(
+    //         JSON.stringify(
+    //             jsonModif.query('class.list[{id:"bh4ebrbvdsdqw"}].data.array[{id:"gw34gobgjieuwcv"}].name', data, {
+    //                 complete: true
+    //             })
+    //         )
+    //     ).to.eql(
+    //         '{"location":["class","list[1]","data","array[2]","name"],"value":"Jack"}'
+    //     )
+    // })
+    // it('query all', () => {
+    //     expect(
+    //         JSON.stringify(
+    //             jsonModif.query('class.list[{name:"nimo"}]', data, {
+    //                 all: true
+    //             })
+    //         )
+    //     ).to.eql(
+    //         JSON.stringify(
+    //             [{"id":"bh4ebrbvdsdqw","name":"nimo","age":24,"data":{"array":[{"id":"egvwegewfwd","name":"Jack"},{"id":"safwegweg","name":"Jack"},{"id":"gw34gobgjieuwcv","name":"Jack"}]}},{"id":"b4wg4w3g3g434g","name":"nimo"}]
+    //         )
+    //     )
+    // })
+    // it('query all complete', () => {
+    //     expect(
+    //         JSON.stringify(
+    //             jsonModif.query('class.list[{name:"nimo"}]', data, {
+    //                 all: true,
+    //                 complete: true
+    //             })
+    //         )
+    //     ).to.eql(
+    //         [
+    //             {
+    //                 location: ["class","list[1]"],
+    //                 value: {
+    //                     "id": "bh4ebrbvdsdqw",
+    //                     "name": "nimo",
+    //                     "age": 24,
+    //                     "data": {
+    //                         "array": [
+    //                             {
+    //                                 "id": "egvwegewfwd",
+    //                                 "name": "Jack"
+    //                             },
+    //                             {
+    //                                 "id": "safwegweg",
+    //                                 "name": "Jack"
+    //                             },
+    //                             {
+    //                                 "id": "gw34gobgjieuwcv",
+    //                                 "name": "Jack"
+    //                             }
+    //                         ]
+    //                     }
+    //                 }
+    //             },
+    //             {
+    //                 location: ["class","list[2]"],
+    //                 value: {
+    //                     "id": "b4wg4w3g3g434g",
+    //                     "name": "nimo"
+    //                 }
+    //             }
+    //         ]
+    //
+    //      )
+    // })
 })
--- test/queryAll.test.js
+++ test/queryAll.test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import jsonModif from "../lib/index"
 import extend from "safe-extend"
+var flatten = require('array-flatten')
 var data = {
     class: {
         user: {
@@ -27,7 +28,25 @@ var data = {
                     array: [
                         {
                             id: 'ashfuewgfwef',
-                            name: 'Jack'
+                            name: 'Jack1',
+                            age: 1
+                        }
+                    ]
+                }
+            },
+            {
+                id: 'werg3rbherbhre',
+                name: 'abc',
+                data: {
+                    array: [
+                        {
+                            id: 'evgwegeg',
+                            name: 'Jack2'
+                        },
+                        {
+                            id: 'egvwegvwegewg',
+                            name: 'Jack3',
+                            age: 1
                         }
                     ]
                 }
@@ -59,5 +78,108 @@ describe('query', () => {
                 [{"id":"bh4ebrbvdsdqw","name":"nimo","age":24},{"id":"b4wg4w3g3g434g","name":"nimo"}]
             )
         )
+    })
+    it('query all complete', () => {
+        expect(
+            JSON.stringify(
+                jsonModif.queryAll('class.list[{name:"nimo"}]', data, {
+                    complete: true
+                })
+            )
+        ).to.eql(
+            JSON.stringify(
+                [
+                    {
+                        location: [
+                            "class",
+                            "list[2]"
+                        ],
+                        value: {
+                            "id": "bh4ebrbvdsdqw",
+                            "name": "nimo",
+                            "age": 24
+                        }
+                    },
+                    {
+                        location: [
+                            "class",
+                            "list[3]"
+                        ],
+                        value: {
+                            "id": "b4wg4w3g3g434g",
+                            "name": "nimo"
+                        }
+                    }
+                ]
+            )
+        )
+    })
+    // it('query all empy', () => {
+    //
+    // })
+    it('query all multi [JSON]', () => {
+        var data = {
+            list: [
+                {
+                    type: 'read',
+                    comment: [
+                        {
+                            name: 'anonymity',
+                            skills: [
+                                {
+                                    id: '1-1-1',
+                                    html: true,
+                                    css: false
+                                },
+                                {
+                                    id: '1-1-2',
+                                    html: false,
+                                    css: true
+                                },
+                                {
+                                    id: '1-1-3',
+                                    html: true,
+                                    css: false
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    type: 'unread'
+                },
+                {
+                    type: 'read'
+                },
+                {
+                    type: 'read',
+                    comment: [
+                        {
+                            name: 'anonymity',
+                            skills: [
+                                {
+
+                                },
+                                {
+                                    id: '3-1-1',
+                                    html: false,
+                                    css: false
+                                },
+                                {
+                                    id: '3-1-2',
+                                    html: true,
+                                    css: true
+                                },
+                                {
+                                    id: '3-1-3',
+                                    html: false,
+                                    css: false
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
     })
 })
